28d6023f0e34ce0ec1f06de4046d18a1a1759426
sayevvv <shamilbasayev10@gmail.com>
2023-10-03 13:57:01 +0700

    Menambahkan input 2000 tahun kabisat

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -7,13 +7,15 @@ public class Pemilihan2Percobaan101 {
         tahun = input01.nextInt();
 
         if (tahun % 4 == 0) {
-            if (tahun % 100 == 0 && tahun % 400 == 0) {
+           if (tahun % 100 == 0) {
+            if (tahun % 400 == 0) {
                 System.out.println("Tahun Kabisat");
-            } else if (tahun % 100 == 0) {
-                System.out.println("Bukan Tahun Kabisat");
             } else {
-                System.out.println("Tahun Kabisat");
+                System.out.println("Bukan Tahun Kabisat");
             }
+           } else {
+            System.out.println("Tahun Kabisat");
+           }
             } else {
                 System.out.println("Bukan Tahun Kabisat");
             } input01.close();
