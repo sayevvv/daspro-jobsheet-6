9a87a0e00494165c7d3582781083e2ca380e7b4e
sayevvv <shamilbasayev10@gmail.com>
2023-10-03 09:01:01 +0700

    Menambahkan file Pemilihan2Percobaan

---
+++ Pemilihan2Percobaan101.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan101 {
+    public static void main(String[] args) {
+        int tahun;
+        Scanner input01 = new Scanner(System.in);
+        System.out.print("Input tahun : ");
+        tahun = input01.nextInt();
+
+        if (tahun % 4 == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+        } else 
+            System.out.println("Bukan Tahun Kabisat");
+        input01.close();
+    }
+}
