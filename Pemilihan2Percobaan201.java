0d86688f86f9e182676a5c97322ed20351ffa64d
sayevvv <shamilbasayev10@gmail.com>
2023-10-03 14:33:39 +0700

    Modifikasi semua bentuk segitiga

--- Pemilihan2Percobaan201.java
+++ Pemilihan2Percobaan201.java
@@ -16,9 +16,12 @@ public class Pemilihan2Percobaan201 {
         if (totalSudut == 180) {
             if ((sudut1 == 90) || (sudut2 == 90) || (sudut3 == 90)) 
                 System.out.println("Segitiga tersebut adalah segitiga siku-siku");
-             else 
-                System.out.println("Segitiga tersebut adalah bukan segitiga siku-siku");
-            
+             else if ((sudut1 == 60) && (sudut2 == 60) && (sudut3 == 60)) 
+                System.out.println("Segitiga tersebut adalah segitiga sama sisi");
+            else if ((sudut1 == sudut2) || (sudut1 == sudut3) || (sudut2 == sudut3))
+                System.out.println("Segitiga tersebut adalah segitiga sama kaki");
+            else 
+            System.out.println("Segitiga tersebut adalah segitiga sembarang");
         } else {
             System.out.println("Bukan Segitiga"); }
             input01.close();
