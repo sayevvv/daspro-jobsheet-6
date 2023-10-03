69d504c405694fea20beeb9e7e3407f7fa6e1ddf
sayevvv <shamilbasayev10@gmail.com>
2023-10-03 10:01:52 +0700

    Modifikasi input 2000

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -7,15 +7,15 @@ public class Pemilihan2Percobaan101 {
         tahun = input01.nextInt();
 
         if (tahun % 4 == 0) {
-            if (tahun % 100 == 0) {
+            if (tahun % 100 == 0 && tahun % 400 == 0) {
+                System.out.println("Tahun Kabisat");
+            } else if (tahun % 100 == 0) {
                 System.out.println("Bukan Tahun Kabisat");
-            }
-            else {
+            } else {
                 System.out.println("Tahun Kabisat");
             }
-    } else {
-        System.out.println("Tahun Kabisat");
+            } else {
+                System.out.println("Bukan Tahun Kabisat");
+            } input01.close();
+        } 
     }
-    input01.close();
-}
-}
