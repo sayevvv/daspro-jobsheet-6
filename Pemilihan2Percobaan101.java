8f62b9a1006b6b206d0b1f51f8ca9135102e426c
sayevvv <shamilbasayev10@gmail.com>
2023-10-03 09:15:32 +0700

    Modifikasi input 2100 bukan tahun Kabisat

--- Pemilihan2Percobaan101.java
+++ Pemilihan2Percobaan101.java
@@ -7,10 +7,15 @@ public class Pemilihan2Percobaan101 {
         tahun = input01.nextInt();
 
         if (tahun % 4 == 0) {
-            if ((tahun % 100) != 0)
-            System.out.println("Tahun Kabisat");
-        } else 
-            System.out.println("Bukan Tahun Kabisat");
-        input01.close();
+            if (tahun % 100 == 0) {
+                System.out.println("Bukan Tahun Kabisat");
+            }
+            else {
+                System.out.println("Tahun Kabisat");
+            }
+    } else {
+        System.out.println("Tahun Kabisat");
     }
+    input01.close();
+}
 }
